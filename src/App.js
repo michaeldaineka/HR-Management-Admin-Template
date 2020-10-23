33e97b9e748fca50fda3b1d8931292ad254fbff0
Michael Daineka <michaeldaineka@gmail.com>
2020-10-23 21:16:04 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Button} from 'antd'
+import './App.css';
+
+function App() {
+  return (
+      <Button type="primary">Primary Button</Button>
+  );
+}
+
+export default App;
