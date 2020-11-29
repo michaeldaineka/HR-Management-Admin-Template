e2da28dd3d1b04e7a27de6b5624c105b1cc1357f
Michael Daineka <michaeldaineka@gmail.com>
2020-11-29 22:29:42 +0300

    feat: Login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,30 +1,33 @@
 import React from "react";
-import { Button, Layout, Row, Col, Tabs } from "antd";
+import { Button, Input, Row, Col, Tabs, Space, Typography } from "antd";
 import s from "./Login.module.less";
 import logo from "images/logo.svg";
 
 export default () => {
   const { TabPane } = Tabs;
+  const {Title} = Typography;
   return (
     <Row>
       <Col sm={24} md={12}>
         <section className={s.loginSection}>
-          <Tabs defaultActiveKey="1" tabPosition={"bottom"}>
-            <TabPane tab="Tab 1" key="1">
-              Content of Tab Pane 1
+          <Tabs defaultActiveKey="1" tabPosition={"bottom"} centered>
+            <TabPane tab="Login" key="1">
+              <Space direction={"vertical"} size={"middle"}>
+                <Input placeholder="Email" />
+                <Input placeholder="Password" />
+                <Button type={"primary"} block>Log In</Button>
+              </Space>
             </TabPane>
-            <TabPane tab="Tab 2" key="2">
+            <TabPane tab="Register" key="2">
               Content of Tab Pane 2
-            </TabPane>
-            <TabPane tab="Tab 3" key="3">
-              Content of Tab Pane 3
             </TabPane>
           </Tabs>
         </section>
       </Col>
       <Col sm={24} md={12}>
         <section className={s.infoSection}>
-          <img src={logo} />
+          <img src={logo} alt={"MoveUp"} />
+          <Title level={1}>HR Management Admin Template</Title>
         </section>
       </Col>
     </Row>
